ab5bc995224484ab866868d390ccae3943472d02
hduarte <29019005+Bacmel@users.noreply.github.com>
2023-05-13 13:25:53 +0200

    ref ♻️ : Change R3 object to np.array

--- state.py
+++ state.py
@@ -5,7 +5,7 @@ class State:
 
     def __init__(self, R, v, p, a_b, ω_b):
         self.R = SO3(R) # rotation
-        self.v = R3(v) # velocity
-        self.p = R3(p) # position
-        self.a_b = R3(a_b) # linear acceleration bias
-        self.ω_b = R3(ω_b) # angular velocity bias
+        self.v = v # velocity
+        self.p = p # position
+        self.a_b = a_b # linear acceleration bias
+        self.ω_b = ω_b # angular velocity bias
