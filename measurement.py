21ae977d2d548e13812924fc5b1fe38622bd0b74
hduarte <hduarte@iri.upc.edu>
2023-05-16 15:22:08 +0200

    feat ✨: Add default state and measurements

--- measurement.py
+++ measurement.py
@@ -4,14 +4,14 @@ from manifpy import SO3, SO3Tangent
 
 class ImuMeasurement:
 
-    def __init__(self, a_m, ω_m):
+    def __init__(self, a_m=np.zeros(3), ω_m=np.zeros(3)):
         self.a_m = a_m  # linear acceleration measurement
         self.ω_m = ω_m  # angular velocity measurement
 
 
 class ImuNoise:
 
-    def __init__(self, a_wn, ω_wn, a_rw, ω_rw):
+    def __init__(self, a_wn=np.zeros(3), ω_wn=np.zeros(3), a_rw=np.zeros(3), ω_rw=np.zeros(3)):
         self.a_wn = a_wn  # linear acceleration white noise
         self.ω_wn = ω_wn  # angular velocity white noise
         self.a_rw = a_rw  # linear acceleration random walk
@@ -20,13 +20,13 @@ class ImuNoise:
 
 class OptitrackMeasurement:
 
-    def __init__(self, R_m, p_m):
+    def __init__(self, R_m=SO3.Identity(), p_m=np.zeros(3)):
         self.R_m = SO3(R_m.coeffs_copy())  # rotation measurement
         self.p_m = p_m  # position measurement
     
 
 class OptitrackNoise:
 
-    def __init__(self, R_wn, p_wn):
-        self.R_wn = SO3Tangent(R_wn.copy())  # rotation white noise
+    def __init__(self, R_wn=SO3Tangent.Zero(), p_wn=np.zeros(3)):
+        self.R_wn = SO3Tangent(R_wn.coeffs_copy())  # rotation white noise
         self.p_wn = p_wn  # position white noise
--- state.py
+++ state.py
@@ -1,9 +1,10 @@
-from manifpy import R3, SO3
+from manifpy import SO3
+import numpy as np
 
 
 class State:
 
-    def __init__(self, R, v, p, a_b, ω_b):
+    def __init__(self, R=SO3.Identity(), v=np.zeros(3), p=np.zeros(3), a_b=np.zeros(3), ω_b=np.zeros(3)):
         self.R = SO3(R.coeffs_copy()) # rotation
         self.v = v # velocity
         self.p = p # position
