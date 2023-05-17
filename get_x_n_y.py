0f6877c79e1c8af1551e01cd9991141ff1db944f
hduarte <hduarte@iri.upc.edu>
2023-05-17 17:10:37 +0200

    fix 🐛: Correct get x & y function

--- get_x_n_y.py
+++ get_x_n_y.py
@@ -3,13 +3,9 @@
 # X_list[i].p[0] # Of i state trhough simulated, x-coord position
 # X_list[i].p[1] # Of i state trhough simulated, y-coord position
 
-x = []
-y = []
-
 def get_X_n_Y (X_list):
-    for i in range(0,len(X_list)-1):
-        x.append(X_list[i].p[0])
-        y.append(X_list[i].p[1])
+    x = [x_i.p[0] for x_i in X_list]
+    y = [x_i.p[1] for x_i in X_list]
     return x,y
 
 
